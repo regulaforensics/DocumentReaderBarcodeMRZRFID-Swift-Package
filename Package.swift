15acdbc615d9a03ff5117bcf86bb0d9b6fd695ad
jenkins <jenkins@regula.by>
2025-03-29 00:19:22 +0300

    Release 7.6.11787

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
             targets: ["BarcodeMRZRFID"]),
     ],
     targets: [
-        .binaryTarget(name: "BarcodeMRZRFID", url: "https://pods.regulaforensics.com/BarcodeMRZRFID/7.5.11012/DocumentReaderCore_barcodemrzrfid_7.5.11012.zip", checksum: "995c4bfa76e2584b5b23e478003b689107e37eeac6dc7a1357147b7b9b5083aa"),
+        .binaryTarget(name: "BarcodeMRZRFID", url: "https://pods.regulaforensics.com/BarcodeMRZRFID/7.6.11787/DocumentReaderCore_barcodemrzrfid_7.6.11787.zip", checksum: "dbc65c7bcef6d71646005a344de3d17692dc5728ff75740458e91ab9ef392b0d"),
     ]
 )
